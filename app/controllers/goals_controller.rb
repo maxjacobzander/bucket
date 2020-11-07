02458c845142066d9c4c04bc908ae584c942c6b9
maxjacobzander <maxjacobzander@gmail.com>
2020-11-07 16:51:08 -0600

    GoalsController index and new

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,2 +1,15 @@
 class GoalsController < ApplicationController
+
+    def index
+        @goals = Goal.all
+    end
+
+    def new
+        @goal = Goal.new(goal_params)
+        if @goal.save
+            redirect_to @goal
+        else
+            render :new
+        end
+    end
 end
