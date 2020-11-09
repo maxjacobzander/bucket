b4c544e5508982563fad964a7be55e74850726ec
maxjacobzander <maxjacobzander@gmail.com>
2020-11-08 18:57:50 -0600

    GoalsController and BucketList Controller expanded and views added

--- app/controllers/bucket_lists_controller.rb
+++ app/controllers/bucket_lists_controller.rb
@@ -1,2 +1,28 @@
 class BucketListsController < ApplicationController
+
+    def index
+        @bucketlists = BucketList.all
+    end
+
+    def new
+        @bucketlist = BucketList.new
+    end
+
+    def create
+        @bucketlist = Bucketlist.new
+        if @bucketlist.save
+            redirect_to @bucketlist
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @bucketlist = 
+
+    end
+
+    def update
+
+    end
 end
--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -5,11 +5,22 @@ class GoalsController < ApplicationController
     end
 
     def new
+        @goal = Goal.new
+    end
+
+
+    def create
         @goal = Goal.new(goal_params)
         if @goal.save
             redirect_to @goal
         else
             render :new
         end
+    end
+
+    private
+
+    def goal_params
+        params.require(:goal).permit(:name)
     end
 end
