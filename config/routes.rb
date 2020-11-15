d7f4c26d3a04105760b4b49b03ac2e8df6bc1896
maxjacobzander <maxjacobzander@gmail.com>
2020-11-14 18:44:58 -0600

    nested path on bucket show fixed

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   end
   devise_for :users, :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :bucket_lists
+  end
 
   root to: "bucket_lists#index"
 end
