6839240ae28cef7b85cf23fc92376595971a3245
maxjacobzander <maxjacobzander@gmail.com>
2020-11-18 11:12:59 -0600

    removed pry from omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
 
   def self.from_omniauth(auth)
-    binding.pry
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.name = auth.info.name
